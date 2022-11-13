336e6b3c578ecb03680f42bfa1bd2f269c9c76e4
dewiika63 <dewiika6303@gmail.com>
2022-11-13 21:07:32 +0700

    first commit

---
+++ lib/controller/demoController.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:minggu_9/model/product.dart';
+import 'package:get/get.dart';
+
+class DemoController extends GetxController {
+  var cartItems = <Product>[].obs;
+  int get cartCount => cartItems.length;
+  double get totalAmount =>
+      cartItems.fold(0.0, (sum, element) => sum + element.price);
+  addToCart(Product product) {
+    cartItems.add(product);
+  }
+
+  final Storage = GetStorage();
+  bool get isDark => Storage.read('darkmode') ?? false;
+  ThemeData get Theme => isDark ? ThemeData.dark() : ThemeData.light();
+  void changeTheme(bool val) => Storage.write('darkmode', val);
+}
